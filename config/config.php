1f783b8d9c27299281304f441b554e0810d67548
cesarevalo <arevalo.cesar@gmail.com>
2025-08-21 11:18:20 -0400

    Delete config directory

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Archivo: config/config.php
-
-// Define la URL base de tu proyecto. 
-// Es muy importante que termine con una barra inclinada /
-define('BASE_URL', 'https://topperfumesccs.com/EssenceManager/');
-
-?>
