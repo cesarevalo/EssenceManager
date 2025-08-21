1151b8c1b9b09c747a6023f4039bfbdba10eb9c9
cesarevalo <arevalo.cesar@gmail.com>
2025-08-21 11:18:36 -0400

    Delete public directory

--- public/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// public/admin/logout.php
-session_start();
-$_SESSION = array();
-session_destroy();
-header('Location: login.php');
-exit;
-?>
--- public/logout_client.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION = array(); // Limpiar el array de sesión
-session_destroy();  // Destruir la sesión
-header('Location: catalog.php');
-exit;
-?>
